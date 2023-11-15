ee8500290f98c22d0ae100e7d246afee8800d73a
Apple <appleeyespackage@gmail.com>
2023-11-15 23:09:46 +0100

    #14 Create Progress entity using maker bundle

---
+++ src/Entity/Progress.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProgressRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProgressRepository::class)]
+class Progress
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(name: "enrollment_id")]
+    private ?int $enrollmentId = null;
+
+    #[ORM\Column(name: "lesson_id")]
+    private ?int $lessonId = null;
+
+    #[ORM\Column]
+    private ?int $status = null;
+
+    #[ORM\Column(name: "last_accessed")]
+    private ?\DateTimeImmutable $lastAccessed = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getEnrollmentId(): ?int
+    {
+        return $this->enrollmentId;
+    }
+
+    public function setEnrollmentId(int $enrollmentId): static
+    {
+        $this->enrollmentId = $enrollmentId;
+
+        return $this;
+    }
+
+    public function getLessonId(): ?int
+    {
+        return $this->lessonId;
+    }
+
+    public function setLessonId(int $lessonId): static
+    {
+        $this->lessonId = $lessonId;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getLastAccessed(): ?\DateTimeImmutable
+    {
+        return $this->lastAccessed;
+    }
+
+    public function setLastAccessed(\DateTimeImmutable $lastAccessed): static
+    {
+        $this->lastAccessed = $lastAccessed;
+
+        return $this;
+    }
+}
