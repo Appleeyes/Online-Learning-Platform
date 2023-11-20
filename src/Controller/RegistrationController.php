bb9b225e98ab485fb8bc49915c8ef645d6f63614
Apple <appleeyespackage@gmail.com>
2023-11-21 00:23:05 +0100

    feat: create component for roles and permissions

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,6 +40,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            $user->setRoles(['ROLE_STUDENT']);
 
             $entityManager->persist($user);
             $entityManager->flush();
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,10 +6,12 @@ use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     public function index(): Response
     {
         $user = $this->getUser();
@@ -21,6 +23,7 @@ class UserController extends AbstractController
     #[Route('/users/all', name: 'app_all_users')]
     public function users(UsersRepository $usersRepisotory): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_INSTRUCTOR');
         $users = $usersRepisotory->findAll();
         return $this->render('user/all_users.html.twig', [
             'users' => $users
