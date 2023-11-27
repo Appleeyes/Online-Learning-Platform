86cc72af1c46cf2de58ccc2c24e21b24edd58e9f
Apple <appleeyespackage@gmail.com>
2023-11-27 22:03:58 +0100

    #51 Create CodeException acceptance tests for Login form

---
+++ tests/acceptance/LoginCest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\acceptance;
+
+use App\Tests\AcceptanceTester;
+
+class LoginCest
+{
+    public function loginpageWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/login');
+        $I->seeElement('#username');
+        $I->seeElement('#password');
+        $I->seeElement('button[type="submit"]');
+    }
+
+    public function loginFormFailedOnWrongCredentials(AcceptanceTester $I)
+    {
+        $I->amOnPage('/login');
+        $I->fillField('_username', 'TestusernameAcceptance1');
+        $I->fillField('_password', '123456');
+        $I->click('button[type="submit"]');
+        $I->see('Invalid credentials.');
+    }
+
+    public function aloginFormWorks(AcceptanceTester $I)
+    {
+        $I->amOnPage('/login');
+        $I->fillField('_username', 'TestusernameAcceptance');
+        $I->fillField('_password', '123456');
+        $I->click('button[type="submit"]');
+        $I->see('Create profile');
+    }
+}
